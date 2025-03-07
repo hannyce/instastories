7ac3a39f72cddd00693980f95ce4bad1046215e9
Anis BOUZIANE <anisb@mawaqit.net>
2025-03-07 11:57:37 +0100

    remove samples and add like key for the entity

--- InstaStories/Features/StoryList/Domain/Entity/User.swift
+++ InstaStories/Features/StoryList/Domain/Entity/User.swift
@@ -14,26 +14,13 @@ class User: Identifiable {
     var username: String
     var imagePath: String
     var seen: Bool
+    var liked: Bool
 
-    init(id: String, username: String, imagePath: String, seen: Bool = false) {
+    init(id: String, username: String, imagePath: String, seen: Bool = false, liked: Bool = false) {
         self.id = id
         self.username = username
         self.imagePath = imagePath
         self.seen = seen
-    }
-}
-
-extension User {
-   static var sample: [User] {
-        [
-            User(id: "1", username: "user1", imagePath: "person.crop.circle.fill", seen: Bool.random()),
-            User(id: "1", username: "user2", imagePath: "person.crop.circle.fill", seen: Bool.random()),
-            User(id: "1", username: "user3", imagePath: "person.crop.circle.fill", seen: Bool.random()),
-            User(id: "1", username: "user4", imagePath: "person.crop.circle.fill", seen: Bool.random()),
-            User(id: "1", username: "user5", imagePath: "person.crop.circle.fill", seen: Bool.random()),
-            User(id: "1", username: "user6", imagePath: "person.crop.circle.fill", seen: Bool.random()),
-            User(id: "1", username: "user7", imagePath: "person.crop.circle.fill", seen: Bool.random()),
-            User(id: "1", username: "user8", imagePath: "person.crop.circle.fill", seen: Bool.random())
-        ]
+        self.liked = liked
     }
 }
